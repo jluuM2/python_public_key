d49538c4888e58163ed42454e2de8fd4a27c4806
nikolaspaci <nikolaspaci>
2021-03-05 11:01:18 +0100

    ajout objet c++

--- hello/hello_component.cpp
+++ hello/hello_component.cpp
@@ -10,13 +10,36 @@ char const* greet() {
   return "hello, world";
 }
 
+class Voiture
+{
+    public:
+        Voiture(const std::string &nom, int litreEssence) : nom(nom), litreEssence(litreEssence) {}
+        ~Voiture() {}
+
+        void rouler() { litreEssence--; }
+        const std::string &get_nom() const { return nom; }
+        int get_litreEssence() const { return litreEssence; }
+		void setEssence(int &essence_) { litreEssence = essence_; }
+
+    private:
+        std::string nom;
+        int litreEssence;
+};
+ 
 namespace py = pybind11;
 
 
 PYBIND11_MODULE(hello_component,greetings)
 {
   greetings.doc() = "greeting_object 1.0";
-  
   greetings.def("greet", &greet, "a function saying hello");
   greetings.def("getVersion", &getVersion, "a function returning the version");
+  
+   // bindings to Voiture class
+    py::class_<Voiture>(greetings, "Voiture", py::dynamic_attr())
+        .def(py::init<const std::string &, int>())
+        .def("rouler", &Voiture::rouler)
+        .def("get_litreEssence", &Voiture::get_litreEssence)
+        .def("get_nom", &Voiture::get_nom)
+		.def("setEssence", &Voiture::setEssence);
 }
